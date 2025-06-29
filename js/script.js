c5265fbc138bffefdaf8c73b5694df090ff11381
Katherine <freepthanq1@gmail.com>
2025-06-29 13:53:39 +0300

    view all as diferent blocks

--- js/script.js
+++ js/script.js
@@ -24,23 +24,36 @@ hamNenu.addEventListener('click', () => {
 document.addEventListener('DOMContentLoaded', function () {
     const dealButtons = document.querySelectorAll('.tabs-deals__button');
     const tabsItems = document.querySelectorAll('.item-tabs__items');
-
+    
     if (dealButtons.length > 0) {
-    // Спочатку додаємо клас active першій кнопці та табу
+        // Спочатку додаємо клас active першій кнопці та табу
         dealButtons[0].classList.add('active');
         tabsItems[0].classList.add('active');
-
-    // Додаємо обробник подій на всі кнопки
+        
+        // Додаємо обробник подій на всі кнопки
         dealButtons.forEach((button, index) => {
             button.addEventListener('click', function () {
-        // При кліку прибираємо active у всіх
+                // При кліку прибираємо active у всіх
                 dealButtons.forEach(btn => btn.classList.remove('active'));
                 tabsItems.forEach(item => item.classList.remove('active'));
-
-        // Додаємо active тільки тій, на яку натиснули
+                
+                // Додаємо active тільки тій, на яку натиснули
                 this.classList.add('active');
                 tabsItems[index].classList.add('active');
             });
         });
     }
+    
+    const headerMainButton = document.querySelector('.header-deals__button');
+    const dealsBody = document.querySelector('.tabs-deals__body');
+
+    if (headerMainButton) {
+        headerMainButton.addEventListener('click', function () {
+            // Прибираємо всі активні кнопки
+            dealButtons.forEach(btn => btn.classList.remove('active'));
+            // Додаємо active до всіх item-tabs__items
+            tabsItems.forEach(item => item.classList.add('active'));
+        });
+    }
 });
+
